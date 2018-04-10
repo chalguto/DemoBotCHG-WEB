5a5e1766b9e6ab97c575fb95433b86ebfe5073d4
Christian Gutiérrez Torres <christian.gutierrez.torres@intercapit.com>
2018-04-10 10:58:51 -0300

    commit

--- public/stylesheets/webChat.js
+++ public/stylesheets/webChat.js
@@ -11,11 +11,11 @@ window.botchatDebug = false;
 
 const speechOptions = {
     speechRecognizer: new CognitiveServices.SpeechRecognizer({
-        subscriptionKey: '9b48f41f9a7e4e2c9588f7b1dfaee2b2'
+        subscriptionKey: '02321889a65047a4b36d4a163f41dd97'
     }),
     speechSynthesizer: new CognitiveServices.SpeechSynthesizer({
         gender: CognitiveServices.SynthesisGender.Female,
-        subscriptionKey: '9b48f41f9a7e4e2c9588f7b1dfaee2b2',
+        subscriptionKey: '02321889a65047a4b36d4a163f41dd97',
         voiceName: 'Microsoft Server Speech Text to Speech Voice (en-ES, JessaRUS)'
     })
 };
